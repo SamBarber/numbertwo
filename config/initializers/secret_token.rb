404f4b043a5668de87acdde08540eaa3fa1e8aee
Sam P. Barber <sam.barber.mail@gmail.com>
2012-11-13 23:39:08 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numbertwo::Application.config.secret_token = '8604b303abd9dfb2cc0931925bdebc280fddb61983339b320494df26762b4d774a43145d8d990f66b8789bbb30ea6183978eae51fc471870c0e3e5ef75e9e957'
